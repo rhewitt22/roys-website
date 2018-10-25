935fce6f6bfe93da873d76e5bfbf14cff36a4606
Roy Hewitt <roy_hewitt@fws.gov>
2018-10-25 08:19:55 -0400

    Configure Gatsby to consume JSON data for GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Roy Hewitt, Web Developer',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -19,12 +19,19 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#112F41',
+        theme_color: '#112F41',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
   ],
 }
