173b97cca6b3ca0e30d39697099d31e25e269259
Roy Hewitt <roy_hewitt@fws.gov>
2018-10-30 18:05:53 -0400

    Add content to about page

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const Courses = ({ data }) => (
+  <Layout>
+    <h1>About</h1>
+    <p>Hi 👋</p>
+
+    <p>My name is Roy Hewitt, I'm a web developer living in Atlanta, GA.</p>
+  </Layout>
+)
+
+export default Courses
