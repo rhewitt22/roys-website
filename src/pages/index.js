ca84a80dc8b81addbc9cc6ba43123727ef2552f0
Roy Hewitt <roy_hewitt@fws.gov>
2018-10-30 18:06:39 -0400

    Update link, add todo

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Image from '../components/image'
 
+// TODO: Create blog listing
+
 const IndexPage = () => (
   <Layout>
     <h1>Hi people</h1>
@@ -12,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/courses/">Go to the courses</Link>
   </Layout>
 )
 
